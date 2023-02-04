0f4e0aec2894e6ffe2942d2df685288a3a25b287
jfair4005 <124472488+jfair4005@users.noreply.github.com>
2023-02-04 16:25:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('New Workout for 2023')
 streamlit.header('Get Big and Strong')
-streamlit.text('Tri"'"s for the guys')
+streamlit.text('Tris for the guys')
 streamlit.text('Curls for the girls')
 streamlit.text('Bonies for the homies')
