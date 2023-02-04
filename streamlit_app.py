54864f0acb0bd826a848b3fe799e325a937034d7
jfair4005 <124472488+jfair4005@users.noreply.github.com>
2023-02-04 16:24:43 -0700

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('New Workout for 2023')
+streamlit.header('Get Big and Strong')
+streamlit.text('Tri"'"s for the guys')
+streamlit.text('Curls for the girls')
+streamlit.text('Bonies for the homies')
