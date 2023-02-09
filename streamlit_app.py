ada070e9046a93266b42f937c2abdcda6b2fc8e5
jfair4005 <124472488+jfair4005@users.noreply.github.com>
2023-02-09 11:38:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,13 +57,29 @@ if streamlit.button('Get Fruit Load List'):
       streamlit.dataframe(my_data_rows)
       
       
-      
+#allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+      with my_cnx.cursor() as my_cur:
+           my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+           return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      back_from_function = insert_row_snowflake(add_my_fruit)
+      streamlit.text(back_from_function)
+   
 streamlit.stop()
 
 
 
 
+fruit_choice = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', fruit_choice)
 
+
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
@@ -76,12 +92,7 @@ streamlit.stop()
 
 
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', fruit_choice)
 
-
-
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 streamlit.title('New Workout for 2023')
 streamlit.header('🦍Get Big and Strong🦍')
